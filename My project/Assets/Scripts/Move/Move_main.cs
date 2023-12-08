85cf5c65610e894923983e77fec7904fd3e1c2d9
GGENOR1 <rustke97@gmail.com>
2023-12-09 02:06:31 +0300

    Add enemy

--- My project/Assets/Scripts/Move/Move_main.cs
+++ My project/Assets/Scripts/Move/Move_main.cs
@@ -14,10 +14,7 @@ public class Move_main : MonoBehaviour
     public Transform GroundCheck;
     public float CheckRadius = 0.5f;
     public LayerMask Ground;
-    
-
     private Camera MainCamera;
-
     private Animator Animator;
     public Animator Anim;
     public bool CanRun = true;
@@ -41,8 +38,6 @@ public class Move_main : MonoBehaviour
     }
     void Update()
     {
-
-
         if (IsMove)
         {
             Move();
@@ -51,41 +46,27 @@ public class Move_main : MonoBehaviour
 
         if (!IsRolling && Input.GetKeyDown(KeyCode.Space))
         {
-            // Запустите Roll переката
-            //isMove = false;
             stamina.DecreasedStamina(15f);
             StartCoroutine(Roll());
         }
-
         Jump();
-        
     }
 
 
     public void Move()
     {
 
-        // Получаем вектор направления относительно камеры
         Vector3 cameraForward = MainCamera.transform.forward;
         Vector3 cameraRight = MainCamera.transform.right;
-
-        // Игнорируем y-компоненты, чтобы движение было только по горизонтали
         cameraForward.y = 0f;
         cameraRight.y = 0f;
         cameraForward.Normalize();
         cameraRight.Normalize();
-
-        // Получаем ввод от пользователя
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        // Вычисляем направление движения на основе ввода и направления камеры
         Vector3 moveDirection = cameraForward * verticalInput + cameraRight * horizontalInput;
         if (moveDirection.magnitude > Mathf.Abs(0.01f)) transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * 500f);
-        // Нормализуем направление движения, чтобы избежать ускорения при движении по диагонали
         moveDirection.Normalize();
-
-        // Перемещаем персонаж в заданном направлении
         Animator.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, 0.2f).magnitude);
         transform.Translate(moveDirection * MoveSpeedWalk * Time.deltaTime, Space.World);
         if (Input.GetKey(KeyCode.LeftControl) && CanRun)
@@ -93,8 +74,6 @@ public class Move_main : MonoBehaviour
             stamina.DecreasedStamina(0.2f);
             Animator.SetFloat("speed", Vector3.ClampMagnitude(moveDirection, 1).magnitude);
             transform.Translate(moveDirection * (MoveSpeedWalk + MoveSpeedRun) * Time.deltaTime, Space.World);
-
-
         }
     }
 
@@ -103,13 +82,8 @@ public class Move_main : MonoBehaviour
     {
         IsRolling = true;
         gameObject.GetComponent<Animator>().SetTrigger("Rolls");
-
-        yield return new WaitForSeconds(waitsecondRoll); // Подождите 1 секунду (время анимации)
-
-        IsRolling = false; // Снимаем флаг после окончания анимаци
-
-
-
+        yield return new WaitForSeconds(waitsecondRoll);
+        IsRolling = false; 
     }
 
     public void Jump()
@@ -118,23 +92,18 @@ public class Move_main : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.LeftShift)&& IsGround && CanJump)
         {
-           
             Anim.SetTrigger("OnGround");
-            //anim.SetBool("OnGroundB", true);
-            //rb.velocity = new Vector3(rb.velocity.y, jumpForce);
             rb.AddForce(Vector3.up * JumpForce);
             print(rb.position);
         }
     }
 
     public float rayLength = 100f;
-    public float capsuleHeight = 0.1f; // Высота капсулы
-    public float capsuleRadius = 0.5f; // Радиус капсулы
+    public float capsuleHeight = 0.1f;
+    public float capsuleRadius = 0.5f; 
     public LayerMask groundedLayer;
     public void CheckingGround() 
-
     {
-
         Vector3 rayStart = transform.position;
         Vector3 rayDirection = Vector3.down;
          
@@ -142,13 +111,10 @@ public class Move_main : MonoBehaviour
         if (Physics.Raycast(rayStart, rayDirection, out hit, rayLength, groundedLayer))
         {
             IsGround = true;
-            
         }
         else
         {
-
             IsGround = false;
-          
         }
     }
 }
