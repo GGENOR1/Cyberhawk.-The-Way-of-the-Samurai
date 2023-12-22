dd74af040152b28fb38f4339e1589231d05d1cdf
GGENOR1 <rustke97@gmail.com>
2023-12-22 18:01:03 +0300

    Сортировка

---
+++ My project/Assets/Project/Scripts/AI/CheseBeh.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CheseBeh : StateMachineBehaviour
+{
+    NavMeshAgent agent;
+    Transform player;
+    float attackRange = 2f;
+    float noVisibleRange = 15f;
+    private Vector3 spawnPoint;
+    bool isAttackingEnrm = false;
+    float attackTimer = 0f;
+    float attackDelay = 10f;
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Transform pointsObject = GameObject.FindGameObjectWithTag("Points").transform;
+        agent = animator.GetComponent<NavMeshAgent>();
+        spawnPoint = animator.GetComponent<EnemyScript>().startPoint;
+        agent.speed = 4;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+   
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        float Distance = Vector3.Distance(animator.transform.position, player.position);
+        float DistanceToHome = Vector3.Distance(animator.transform.position, spawnPoint);
+
+        if (Distance > noVisibleRange)
+        {
+            if (DistanceToHome < 1)
+            {
+                animator.SetFloat("speed", 0f);
+            }
+            else
+            {
+                animator.SetFloat("speed", 0.2f);
+                agent.SetDestination(spawnPoint);
+            }
+        }
+
+        if (isAttackingEnrm)
+        {
+            attackTimer -= Time.deltaTime;
+            //Debug.Log(AttackTimer);
+            Debug.Log(attackTimer);
+            if (attackTimer <= 0)
+            {
+                isAttackingEnrm = false;
+            }
+        }
+
+        if (Distance < attackRange && !isAttackingEnrm)
+        {
+            agent.SetDestination(player.position);
+            animator.SetBool("IsAttaking", true);
+            isAttackingEnrm = true;
+            attackTimer = Random.Range(0, 2f);
+        }
+
+        if (Distance > attackRange & noVisibleRange > Distance)
+        {
+            agent.SetDestination(player.position);
+            //Debug.Log("Надо бежать");
+            animator.SetFloat("speed", 1f);
+        }
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+        agent.speed = 2;
+    }
+
+
+}
